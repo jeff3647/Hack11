9840a2363acb1f5baef76d436e5877538f13d673
zhuxinyishcn <42358759+zhuxinyishcn@users.noreply.github.com>
2018-11-01 12:03:18 -0500

    Update airport.c
    airport

--- airport.c
+++ airport.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "airport.h"
 
@@ -14,17 +13,13 @@ Airport* createAirport(const char* gpsId,
                        int elevationFeet,
                        const char* city,
                        const char* countryAbbrv){
-						   
 	Airport *airport = (Airport *)malloc(sizeof(Airport) * 1);
-	
 	/**Title*/
 	airport->gpsId = (char *)malloc(sizeof(char) * (strlen(gpsId) + 1));
 	strcpy(airport->gpsId, gpsId);
-	
 	/**Type*/
 	airport->type = (char *)malloc(sizeof(char) * (strlen(type) + 1));
 	strcpy(airport->type, type);
-	
 	/**Name*/
 	airport->name = (char *)malloc(sizeof(char) * (strlen(name) + 1));
 	strcpy(airport->name, name);
@@ -121,34 +116,7 @@ char* airportToString(const Airport* a){
     return str;
 
 
-/**Convert degrees to radians*/
-double degreesToRadians(double degree){
-	return degree * M_PI / 180.0;
 }
-/**Air distance calculation*/
-double getAirDistance(const Airport* origin, const Airport* destination){
-	/*if(originLatitude >= - 180 && destinationLatitude <= 180 && originLongitude >= -180 && destinationLongitude <= 180){
-			originLatitude = originLatitude  * M_PI / 180.0;
-			originLongitude = originLongitude * M_PI / 180.0;
-			destinationLatitude = destinationLatitude * M_PI / 180.0;
-			destinationLongitude = destinationLongitude * M_PI / 180.0;
-			return acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) * cos(destinationLongitude - originLongitude)) * 6371;
-	} else {
-		printf("Error... Incorrect input values\n");
-		return 0;
-	}*/
-	//Restrictions ARE WRONG :( IN WEBGRADER
-	
-	double longitude = origin[]
-	
-	originLatitude = originLatitude  * M_PI / 180.0; //Convert degrees to radians
-	originLongitude = originLongitude * M_PI / 180.0;
-	destinationLatitude = destinationLatitude * M_PI / 180.0;
-	destinationLongitude = destinationLongitude * M_PI / 180.0;
-	
-	return acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) * cos(destinationLongitude - originLongitude)) * 6371;
-}
-
 
 
 
