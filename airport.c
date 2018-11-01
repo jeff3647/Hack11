301c966d0b315f86d945271d4a9aa47801265b5a
jeff3647 <38971@jisedu.or.id>
2018-11-01 11:56:11 -0500

    Get airdistance added

--- airport.c
+++ airport.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "airport.h"
 
@@ -13,38 +14,38 @@ Airport* createAirport(const char* gpsId,
                        int elevationFeet,
                        const char* city,
                        const char* countryAbbrv){
-
+						   
 	Airport *airport = (Airport *)malloc(sizeof(Airport) * 1);
-
+	
 	/**Title*/
 	airport->gpsId = (char *)malloc(sizeof(char) * (strlen(gpsId) + 1));
 	strcpy(airport->gpsId, gpsId);
-
+	
 	/**Type*/
 	airport->type = (char *)malloc(sizeof(char) * (strlen(type) + 1));
 	strcpy(airport->type, type);
-
+	
 	/**Name*/
 	airport->name = (char *)malloc(sizeof(char) * (strlen(name) + 1));
 	strcpy(airport->name, name);
-
+	
 	/**Latitude (double)*/
 	airport->latitude = latitude;
-
+	
 	/**Longitude (double)*/
 	airport->longitude = longitude;
-
+	
 	/**Elevation Feet (int)*/
 	airport->elevationFeet = elevationFeet;
-
+	
 	/**City*/
 	airport->city = (char *)malloc(sizeof(char) * (strlen(city) + 1));
 	strcpy(airport->city, city);
-
+	
 	/**countryAbbrv*/
 	airport->type = (char *)malloc(sizeof(char) * (strlen(countryAbbrv) + 1));
 	strcpy(airport->countryAbbrv, countryAbbrv);
-
+	
 	return airport;
 }
 
@@ -58,68 +59,75 @@ void initAirport(Airport* airport,
                  int elevationFeet,
                  const char* city,
                  const char* countryAbbrv){
-
+	
 	/**Title*/
 	airport->gpsId = (char *)malloc(sizeof(char) * (strlen(gpsId) + 1));
 	strcpy(airport->gpsId, gpsId);
-
+	
 	/**Type*/
 	airport->type = (char *)malloc(sizeof(char) * (strlen(type) + 1));
 	strcpy(airport->type, type);
-
+	
 	/**Name*/
 	airport->name = (char *)malloc(sizeof(char) * (strlen(name) + 1));
 	strcpy(airport->name, name);
-
+	
 	/**Latitude (double)*/
 	airport->latitude = latitude;
-
+	
 	/**Longitude (double)*/
 	airport->longitude = longitude;
-
+	
 	/**Elevation Feet (int)*/
 	airport->elevationFeet = elevationFeet;
-
+	
 	/**City*/
 	airport->city = (char *)malloc(sizeof(char) * (strlen(city) + 1));
 	strcpy(airport->city, city);
-
+	
 	/**countryAbbrv*/
 	airport->type = (char *)malloc(sizeof(char) * (strlen(countryAbbrv) + 1));
 	strcpy(airport->countryAbbrv, countryAbbrv);
-
+	
 	return;
 }
 
 
 char* airportToString(const Airport* a){
-
-
-    if (a == NULL) {
-      char *result = (char *)malloc(sizeof(char) * 7);
-      strcpy(result, "(null)");
-      return result;
-    }
-
-    int n = strlen(a->gpsId) + strlen(a->type) + strlen(a->name)+ 10 +10+
-          10  + strlen(a->city)+strlen(a->countryAbbrv);
-
-    // create a result string
-    char *str = (char *)malloc(sizeof(char) * n);
-
-    // format the student into the temporary string
-    sprintf(str, "%s, %s, %s, %lf, %lf,%d, %s,%s)", strlen(a->gpsId) ,
-             strlen(a->type),  (strlena->name),
-          latitude, longitude, elevationFeet,strlen(a->city),strlen(a->countryAbbrv) ;
-
-    // return the result
-    return str;
-
-
+	
 }
 
 
-double getAirDistance(const Airport* origin, const Airport* destination);
+/**Convert degrees to radians*/
+double degreesToRadians(double degree){
+	return degree * M_PI / 180.0;
+}
+/**Air distance calculation*/
+double getAirDistance(const Airport* origin, const Airport* destination){
+	/*if(originLatitude >= - 180 && destinationLatitude <= 180 && originLongitude >= -180 && destinationLongitude <= 180){
+			originLatitude = originLatitude  * M_PI / 180.0;
+			originLongitude = originLongitude * M_PI / 180.0;
+			destinationLatitude = destinationLatitude * M_PI / 180.0;
+			destinationLongitude = destinationLongitude * M_PI / 180.0;
+			return acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) * cos(destinationLongitude - originLongitude)) * 6371;
+	} else {
+		printf("Error... Incorrect input values\n");
+		return 0;
+	}*/
+	//Restrictions ARE WRONG :( IN WEBGRADER
+	
+	double longitude = origin[]
+	
+	originLatitude = originLatitude  * M_PI / 180.0; //Convert degrees to radians
+	originLongitude = originLongitude * M_PI / 180.0;
+	destinationLatitude = destinationLatitude * M_PI / 180.0;
+	destinationLongitude = destinationLongitude * M_PI / 180.0;
+	
+	return acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) * cos(destinationLongitude - originLongitude)) * 6371;
+}
+
+
+
 
 
 double getEstimatedTravelTime(const Airport* stops,
